37c628df55eab0a7d842aaad1458ddc7bfac0431
ArshidaAfsal <arshidak2000@outlook.com>
2025-03-08 12:14:35 +0530

    initial commit

--- HMS/settings.py
+++ HMS/settings.py
@@ -76,18 +76,25 @@ WSGI_APPLICATION = 'HMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'hospitality_db',
+#         'USER':'postgres',
+#         'PORT':5432,
+#         'HOST':'localhost',
+#         'PASSWORD':'1234'
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hospitality_db',
-        'USER':'postgres',
-        'PORT':5432,
-        'HOST':'localhost',
-        'PASSWORD':'1234'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
